b81219ffd9883f5b07ad038d8ed4a55449a27a26
tsengdodo <annie3530tw@gmail.com>
2016-06-15 17:24:12 +0800

    Add files via upload

---
+++ map.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>憨盧美食網-全台美食地圖</title>
+	<style type="text/css">
+	.word{
+		font-family:"微軟正黑體";
+		letter-spacing:5px;
+		text-align:center;
+		font-weight: bold;
+		color:#20B2AA;
+	}
+	</style>
+</head>
+<body>
+	<img src="HanluLove.jpg" title="憨廬愛美食" width="723" height="170" style="display:block; margin:auto" >
+	<br/>
+	<p class="word">快在憨廬美食地圖上點選任一城市，看看有甚麼好ㄘ的吧！</p>
+	<br/>
+	<br/>
+	<img src="taiwan_map.jpg" usemap="#taiwan_map.jpg" style="display:block; margin:auto">
+	<map name="taiwan_map.jpg">
+		<area shape="rect" coords="469,17,534,47" href="KeelungCity.php">
+		<area shape="rect" coords="342,12,407,42" href="TaipeiCity.php">
+		<area shape="rect" coords="506,65,571,95" href="NewTaipeiCity.php">
+		<area shape="rect" coords="288,33,353,63" href="TaoyuanCity.php">
+		<area shape="rect" coords="267,61,332,91" href="HsinchuCountry.php">
+		<area shape="rect" coords="245,89,310,119" href="HsinchuCity.php">
+		<area shape="rect" coords="225,124,290,154" href="MiaoliCounty.php">
+		<area shape="rect" coords="198,155,263,185" href="TaichungCity.php">
+		<area shape="rect" coords="181,185,246,215" href="ChanghuaCounty.php">
+		<area shape="rect" coords="166,217,231,247" href="YunlinCounty.php">
+		<area shape="rect" coords="153,247,218,277" href="ChiayiCounty.php">
+		<area shape="rect" coords="140,277,205,307" href="ChiayiCity.php">
+		<area shape="rect" coords="153,308,218,338" href="TainanCity.php">
+		<area shape="rect" coords="176,349,241,379" href="KaohsiungCity.php">
+		<area shape="rect" coords="211,390,276,420" href="PingtungCounty.php">
+		<area shape="rect" coords="414,322,479,352" href="PingtungCity.php">
+		<area shape="rect" coords="463,215,528,245" href="HualienCounty.php">
+		<area shape="rect" coords="474,173,539,203" href="NantouCounty.php">
+		<area shape="rect" coords="496,109,561,139" href="YilanCounty.php">
+		<area shape="rect" coords="80,42,145,72" href="LienchiangCounty.php">
+		<area shape="rect" coords="32,162,97,192" href="KinmenCounty.php">
+		<area shape="rect" coords="20,334,85,364" href="PenghuCounty.php">
+	</map>
+</body>
+</html>
